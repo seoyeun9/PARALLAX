5fd25e81bbe45f6847b7b0316e970b3f10dcafc3
seoyeun <seoyeun0315@gmail.com>
2026-07-19 18:54:37 +0100

    Update use.js

--- use.js
+++ use.js
@@ -2,10 +2,10 @@ const btnMoveOn = document.getElementById('btnMoveOn');
 
 if (btnMoveOn) {
   btnMoveOn.addEventListener('click', () => {
-
     const currentWinner = sessionStorage.getItem('currentPlayer');
     let playerHealth = JSON.parse(sessionStorage.getItem('playerHealth')) || {};
 
+    // 💡 [수정됨] 치료하니 피가 0.5만큼 찹니다!
     if (currentWinner && playerHealth[currentWinner] !== undefined) {
       playerHealth[currentWinner] = Math.min(1.0, playerHealth[currentWinner] + 0.5);
       sessionStorage.setItem('playerHealth', JSON.stringify(playerHealth));
