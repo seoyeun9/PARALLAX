fc8a50736f488bce9cd7355a407b02788131eb43
seoyeun <seoyeun0315@gmail.com>
2026-07-19 17:03:36 +0100

    Create result.js

---
+++ result.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const btnSuccessMoveOn = document.getElementById('btnMoveOn');
+
+  if (btnSuccessMoveOn) {
+    btnSuccessMoveOn.addEventListener('click', () => {
+      window.location.href = 'idle.html'; 
+    });
+  }
+});
